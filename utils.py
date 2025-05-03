a4d62f5d825b4aa1249e617230283055bcc3ded2
Dinesh Dhanji <dhanjeehartik@gmail.com>
2025-05-04 00:00:10 +0500

    Implement structural updates and optimizations across multiple modules

--- utils.py
+++ utils.py
@@ -31,6 +31,29 @@ def create_dataloader_v1(
     return dataloader
 
 
+def create_dataloader_v2(
+    texts,
+    batch_size=4,
+    max_length=256,
+    stride=128,
+    shuffle=True,
+    drop_last=True,
+    num_workers=0,
+):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV2(texts, tokenizer, max_length, stride)
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers,
+    )
+
+    return dataloader
+
+
 class GPTDatasetV1(Dataset):
     def __init__(self, txt, tokenizer, max_length, stride):
         self.input_ids = []
@@ -45,6 +68,28 @@ class GPTDatasetV1(Dataset):
             target_chunk = token_ids[i + 1 : i + max_length + 1]
             self.input_ids.append(torch.tensor(input_chunk))
             self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+
+
+class GPTDatasetV2(Dataset):
+    def __init__(self, texts, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        for txt in texts:
+            token_ids = tokenizer.encode(txt, allowed_special={"<|endoftext|>"})
+
+            # Create overlapping chunks per text sample
+            for i in range(0, len(token_ids) - max_length, stride):
+                input_chunk = token_ids[i : i + max_length]
+                target_chunk = token_ids[i + 1 : i + max_length + 1]
+                self.input_ids.append(torch.tensor(input_chunk))
+                self.target_ids.append(torch.tensor(target_chunk))
 
     def __len__(self):
         return len(self.input_ids)
